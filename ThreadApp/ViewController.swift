ddc0817d146796f0ae7c7f9be5e02b9206ef1c82
Shailesh Patel <shailesh001@mac.com>
2021-05-06 15:05:51 +0100

    Added multi-threading with Grand Central Dispatch example

--- ThreadApp/ViewController.swift
+++ ThreadApp/ViewController.swift
@@ -3,16 +3,17 @@
 //  ThreadApp
 //
 //  Created by Shailesh Patel on 05/05/2021.
-//
+// Example of Grand Central Dispatch (GCD) for Multi-Threading 
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var spinnerView: UIActivityIndicatorView!
     @IBOutlet weak var resultsTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        spinnerView.hidesWhenStopped = true
     }
 
     func fetchSomethingFromServer() -> String {
@@ -37,16 +38,59 @@ class ViewController: UIViewController {
     }
 
     @IBAction func doButton(_ sender: UIButton) {
+        // Single Threaded application with appropriate yield for other tasks
         let startTime = NSDate()
         self.resultsTextView.text = ""
-        let fetchedData = self.fetchSomethingFromServer()
-        let processedData = self.processData(input: fetchedData)
-        let firstResult = self.calculateFirstResult(processedData)
-        let secondResult = self.calculateSecondResult(processedData)
-        let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
-        self.resultsTextView.text = resultsSummary
-        let endTime = NSDate()
-        print("Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds")
+        spinnerView.startAnimating()
+        let queue = DispatchQueue.global(qos: .default)
+        queue.async {
+            let fetchedData = self.fetchSomethingFromServer()
+            let processedData = self.processData(input: fetchedData)
+            let firstResult = self.calculateFirstResult(processedData)
+            let secondResult = self.calculateSecondResult(processedData)
+            let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+            
+            DispatchQueue.main.async {
+                self.resultsTextView.text = resultsSummary
+                self.spinnerView.stopAnimating()
+            }
+            
+            let endTime = NSDate()
+            print("Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds")
+        }
+    }
+    
+    @IBAction func doGroupButton(_ sender: UIButton) {
+        // Multi-Threaded Application with appropriate yield for other tasks
+        let startTime = Date()
+        self.resultsTextView.text = ""
+        spinnerView.startAnimating()
+        let queue = DispatchQueue.global(qos: .default)
+        queue.async {
+            let fetchData = self.fetchSomethingFromServer()
+            let processedData = self.processData(input: fetchData)
+            var firstResult: String!
+            var secondResult: String!
+            let group = DispatchGroup()
+            
+            queue.async(group: group) {
+                firstResult = self.calculateFirstResult(processedData)
+            }
+            
+            queue.async(group: group) {
+                secondResult = self.calculateSecondResult(processedData)
+            }
+            
+            group.notify(queue: queue) {
+                let resultsSummary = "First: [\(firstResult!)]\nSecond: [\(secondResult!)]"
+                DispatchQueue.main.async {
+                    self.resultsTextView.text = resultsSummary
+                    self.spinnerView.stopAnimating()
+                }
+                let endTime = Date()
+                print("Completed in \(endTime.timeIntervalSince(startTime)) seconds")
+            }
+        }
     }
 }
 
